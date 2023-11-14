dea5702c3a4eaa9df76803c190e3bd43506ed8cc
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2023-11-14 21:53:37 +0700

    membuat fun CustomerDetailsScreen

--- Esjumbo/app/src/main/java/com/example/esjumbo/HalamanFormulir.kt
+++ Esjumbo/app/src/main/java/com/example/esjumbo/HalamanFormulir.kt
@@ -1,2 +1,11 @@
 package com.example.esjumbo
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerDetailsScreen(
+    onConfirmButtonClicked: (String, String, String) -> Unit,
+    onCancelButtonClicked: () -> Unit
+){
+
+}
